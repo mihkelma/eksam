68010a26f998536996f05c9fd9e5f376c9e5c1a9
Mihkel Matson <mihkelmatson@gmail.com>
2016-06-12 16:39:54 +0300

    Upload funktsioon v1

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+$location ="upload";
+
+
+function upload($name, $loc){
+  $allowedExts = array("txt");
+  $extension = explode(".", $_FILES[$name]["name"]);
+  $file_ext = end($extension);
+
+  if (($_FILES[$name]["size"] < 100000) // see on 100kb
+   && in_array($file_ext, $allowedExts)) {
+    // fail õiget tüüpi ja suurusega
+    if ($_FILES[$name]["error"] > 0) {
+      return "";
+    } else {
+      // vigu ei ole
+      if (file_exists($loc."/" . $_FILES[$name]["name"])) {
+        // fail olemas ära uuesti lae, tagasta failinimi
+        return $_FILES[$name]["name"];
+      } else {
+        // kõik ok, aseta pilt
+        move_uploaded_file($_FILES[$name]["tmp_name"], $loc."/" . $_FILES[$name]["name"]);
+        return $_FILES[$name]["name"];
+      }
+    }
+  } 
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>eksam_mmatson</title>
+    <style type="text/css">
+    </style>
+</head>
+<body>
+      
+      <form action = "" method = "POST" enctype = "multipart/form-data">
+         Vali fail: <input type = "file" name = "fail" />
+         <input type = "submit"/>
+      </form>
+<?php
+if(($_SERVER['REQUEST_METHOD'] == 'POST') &&  isset($_FILES['fail'])) {
+    $uploadfail = upload("fail",$location);
+    if ($uploadfail != "") {
+        echo "<p>Fail ". $uploadfail. " edukalt &uuml;les laetud!</p>";
+    } else {
+        echo "<p>Faili &uuml;leslaadimine ebaonnestus</p>";
+    }
+}
+?>
+
+</body>
+</html>
